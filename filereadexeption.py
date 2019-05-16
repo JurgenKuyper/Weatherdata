edcf0d8df8e4afa0b08e1110533fddbacdf19cf7
BuildTools <unconfigured@null.spigotmc.org>
2019-05-16 08:42:25 +0200

    changed stuff

--- filereadexeption.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-    os.remove("running.txt")
-except FileNotFoundError:
-    print("done")
-# import json
-# with open("datastore.json", "r") as File:
-#     print(json.loads(File.read()))
